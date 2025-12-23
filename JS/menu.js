6d633d597bb554f6e8198648c2551f01da866052
EnnioGianni <gianniennio5@gmail.com>
2025-12-23 22:09:19 +0100

    1

--- JS/menu.js
+++ JS/menu.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   const MENU_LINKS = [
-    { label: "Accueil", href: "../index.html" },
-    { label: "Villes A", href: "../Ville_A/sommaireA.html" },
-    { label: "Villes B", href: "../Ville_B/sommaireB.html" },
-    { label: "Villes C", href: "../Ville_C/sommaireC.html" }
+    { label: "Accueil", href: "./index.html" },
+    { label: "Villes A", href: "../../Ville_A/sommaireVilleA.html" },
+    { label: "Villes B", href: "./Ville_B/sommaireB.html" },
+    { label: "Villes C", href: "./Ville_C/sommaireC.html" }
   ];
 
   /* Injection menu */
